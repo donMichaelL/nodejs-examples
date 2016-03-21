ffdc81dbb73826781ed8c166f0457c547d064e61
Michael <michael.loukeris@icloud.com>
2016-03-21 10:29:42 +0200

    Finish 1 week leader

--- node-express/server-exp-5.js
+++ node-express/server-exp-5.js
@@ -2,7 +2,8 @@ var express = require('express');
 var morgan = require('morgan');
 
 var dishRouter = require('./my_modules/dishRouter');
-var promoRouter = require('./my_modules/promoRouter.js');
+var promoRouter = require('./my_modules/promoRouter');
+var leaderRouter = require('./my_modules/leaderRouter');
 
 var hostname = 'localhost';
 var port = 3000;
@@ -15,6 +16,7 @@ app.use(express.static(__dirname + '/public'));
 // Routing
 app.use('/dishes', dishRouter);
 app.use('/promotions', promoRouter);
+app.use('/leaders', leaderRouter);
 
 app.listen(port, hostname, function(){
 	console.log('Listening in ' + hostname + ':' + port);
